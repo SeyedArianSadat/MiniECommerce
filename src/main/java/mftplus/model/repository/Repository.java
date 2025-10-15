315d2540f8ca08d8242339d86b2bda913268740e
seyedariansadat <ariansadat733@gmail.com>
2025-10-15 15:39:03 +0330

    Repository interface  added

---
+++ src/main/java/mftplus/model/repository/Repository.java
@@ -0,0 +1,13 @@
+package mftplus.model.repository;
+
+import java.util.List;
+
+public interface Repository<T, I> {
+        void save(T t) throws  Exception;
+        void edit(T t) throws Exception;
+        void delete(I id) throws Exception;
+        List<T> findAll() throws Exception;
+        T findById(I id) throws Exception;
+    }
+
+
